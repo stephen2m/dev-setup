56e1097da82c0c51e6b659873cd374d1d2630c63
Stephen Mue <stephenmue@gmail.com>
2019-04-20 22:41:10 +0300

    tweak npm setup script

--- tasks/common/03_npm.sh
+++ tasks/common/03_npm.sh
@@ -29,9 +29,9 @@ fi
 
 if [[ ${userResponse} =~ ^[Yy]$ || ${CIRCLECI} ]]; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+  nvm install --lts
   export NVM_DIR="$HOME/.nvm"
   [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
-  nvm install --lts
   sudo npm install -g @angular/cli tslint
 fi
 
