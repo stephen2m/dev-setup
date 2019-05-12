53356db14fa075781382f3c7354f1e0fb4b12be3
Stephen Mutua <stephenmue@gmail.com>
2019-05-12 21:23:51 +0300

    skip installing java if on cirleci (takes too long) and update git basics script

--- tasks/common/02_java.sh
+++ tasks/common/02_java.sh
@@ -10,44 +10,48 @@
 # shellcheck disable=SC1091
 . common.sh
 
-_outputMessage "Started java installation script $(basename "$0")"
+if [[  ! ${CIRCLECI} ]]; then
+    _outputMessage "Started java installation script $(basename "$0")"
 
-if [[ ${CIRCLECI} ]]; then
-  _outputMessage "Skipping sudo check for circleci"
+    if [[ ${CIRCLECI} ]]; then
+      _outputMessage "Skipping sudo check for circleci"
+    else
+      _hasSudo
+
+      # keep existing `sudo` timestamp until the script is completed
+      while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+    fi
+
+    _outputMessage "Installing JAVA and related helpers"
+
+    if [[ $(_getLinuxVersion) == "ManjaroArch" ]]; then
+      JAVA_VERSION=${JAVA_VERSION_ARCH}
+    else
+      JAVA_VERSION=${JAVA_VERSION_UBUNTU}
+    fi
+
+    if [[ $(_getLinuxVersion) == "/usr/bin/java" ]]; then
+      _outputMessage "Skipping installing JAVA as there's already an installed version: $(java -version 2>&1 | head -n 1)"
+    else
+      if [[ $(_ask "Do you wish to install $JAVA_VERSION?" "Y") ]]; then
+        _installPackage ${JAVA_VERSION}
+      fi
+    fi
+
+    if [[ $(_ask "Do you wish to install sdkman?" "Y") ]]; then
+      curl -s ${SDKMAN_URL} | bash
+      if [[ ${CIRCLECI} != true ]]; then
+        source "$HOME/.sdkman/bin/sdkman-init.sh"
+      fi
+    fi
+
+    if [[ $(_ask "Do you wish to install springboot, gradle and groovy?" "Y") ]]; then
+      sdk install springboot
+      sdk install gradle
+      sdk install groovy
+    fi
 else
-  _hasSudo
-
-  # keep existing `sudo` timestamp until the script is completed
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-fi
-
-_outputMessage "Installing JAVA and related helpers"
-
-if [[ $(_getLinuxVersion) == "ManjaroArch" ]]; then
-  JAVA_VERSION=${JAVA_VERSION_ARCH}
-else
-  JAVA_VERSION=${JAVA_VERSION_UBUNTU}
-fi
-
-if [[ $(_getLinuxVersion) == "/usr/bin/java" ]]; then
-  _outputMessage "Skipping installing JAVA as there's already an installed version: $(java -version 2>&1 | head -n 1)"
-else
-  if [[ $(_ask "Do you wish to install $JAVA_VERSION?" "Y") ]]; then
-    _installPackage ${JAVA_VERSION}
-  fi
-fi
-
-if [[ $(_ask "Do you wish to install sdkman?" "Y") ]]; then
-  curl -s ${SDKMAN_URL} | bash
-  if [[ ${CIRCLECI} != true ]]; then
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
-  fi
-fi
-
-if [[ $(_ask "Do you wish to install springboot, gradle and groovy?" "Y") ]]; then
-  sdk install springboot
-  sdk install gradle
-  sdk install groovy
+  _outputMessage "Skipping java setup"
 fi
 
 _scriptCompletedMessage
