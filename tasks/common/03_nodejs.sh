3e8d835466ae7e1e735f93639365e9d2c66bfbc6
Stephen Mutua <stephenmue@gmail.com>
2019-05-15 20:40:16 +0300

    skip nodejs installation if on circleci

--- tasks/common/03_nodejs.sh
+++ tasks/common/03_nodejs.sh
@@ -14,25 +14,29 @@ start_sec=$(/bin/date +%s.%N)
 
 _writeHeader "NodeJS and NPM Setup"
 
-if [[ $(npm --version) && $(node --version) ]]; then
-  _outputMessage "Skipping installing NodeJS NPM as there's already an installed version: node $(node -v) and NPM v$(npm -v)"
-else
-  _outputMessage "Installing NPM and related helpers"
+if [[ ! ${CIRCLECI} ]]; then
+  if [[ $(npm --version) && $(node --version) ]]; then
+    _outputMessage "Skipping installing NodeJS NPM as there's already an installed version: node $(node -v) and NPM v$(npm -v)"
+  else
+    _outputMessage "Installing NPM and related helpers"
 
-  if _ask "Do you wish to install npm?" Y; then
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    # shellcheck disable=SC1090
-    [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
-    nvm install node
-    nvm use $(node -v)
-    nvm alias default $(node -v)
-    _outputMessage "Installed node $(node -v) and NPM v$(npm -v) successfully."
-    _outputMessage "Your default node version is node $(node -v)."
-    _outputMessage "Installing ANgular CLI & tslint globally."
-    npm install -g @angular/cli tslint && \
-    _outputMessage "Installed Angular CLI and tslint successfully"
+    if _ask "Do you wish to install npm?" Y; then
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+      export NVM_DIR="$HOME/.nvm"
+      # shellcheck disable=SC1090
+      [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
+      nvm install node
+      nvm use $(node -v)
+      nvm alias default $(node -v)
+      _outputMessage "Installed node $(node -v) and NPM v$(npm -v) successfully."
+      _outputMessage "Your default node version is node $(node -v)."
+      _outputMessage "Installing Angular CLI & tslint globally."
+      npm install -g @angular/cli tslint && \
+      _outputMessage "Installed Angular CLI and tslint successfully"
+    fi
   fi
+else
+  _outputMessage "Found env variable CIRCLECI and skipped NodeJS and NPM installation"
 fi
 
 _scriptCompletedMessage ${start_sec}
